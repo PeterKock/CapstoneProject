3cfad254534be098790f45c982e0f12b84eaeaec
pkdev <ppkock@gmail.com>
2025-08-08 11:12:14 +0200

    Create TradeAccountService interface with deposit and withdraw method definitions

--- service/TradeAccountService.java
+++ service/TradeAccountService.java
@@ -1,1 +1,23 @@
 package service;
+
+import java.math.BigDecimal;
+
+/**
+ * Interface for deposit and withdrawal operations on trade accounts.
+ */
+public interface TradeAccountService {
+
+    /**
+     * Deposits the specified amount into the account with the given ID.
+     * @param id account ID
+     * @param amount amount to deposit
+     */
+    void deposit(String id, BigDecimal amount);
+
+    /**
+     * Withdraws the specified amount from the account with the given ID.
+     * @param id account ID
+     * @param amount amount to withdraw
+     */
+    void withdraw(String id, BigDecimal amount);
+}
