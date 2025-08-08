4c1224cb70f383e3d87da6f69c1f9f828ecb40b8
pkdev <ppkock@gmail.com>
2025-08-08 10:46:00 +0200

    Add main method to test cloning of CashAccount and MarginAccount instances

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
+import pojo.CashAccount;
+import pojo.MarginAccount;
+import java.math.BigDecimal;
+
+/**
+ * Main class for testing the cloning functionality of TradeAccount subclasses.
+ */
 public class Main {
     public static void main(String[] args) {
-        
+
+        // Create a CashAccount instance
+        CashAccount cashAccount = new CashAccount("C123", new BigDecimal("1000.00"));
+        System.out.println("Cash Account Details:");
+        System.out.println("ID: " + cashAccount.getId());
+        System.out.println("Cash Balance: " + cashAccount.getCashBalance());
+
+        // Clone the CashAccount
+        CashAccount clonedCashAccount = (CashAccount) cashAccount.clone();
+        System.out.println("Cloned Cash Account Details:");
+        System.out.println("ID: " + clonedCashAccount.getId());
+        System.out.println("Cash Balance: " + clonedCashAccount.getCashBalance());
+
+        System.out.println();
+
+        // Create a MarginAccount instance
+        MarginAccount marginAccount = new MarginAccount("M456", new BigDecimal("5000.00"));
+        System.out.println("Margin Account Details:");
+        System.out.println("ID: " + marginAccount.getId());
+        System.out.println("Margin: " + marginAccount.getMargin());
+
+        // Clone the MarginAccount
+        MarginAccount clonedMarginAccount = (MarginAccount) marginAccount.clone();
+        System.out.println("Cloned Margin Account Details:");
+        System.out.println("ID: " + clonedMarginAccount.getId());
+        System.out.println("Margin: " + clonedMarginAccount.getMargin());
     }
 }
