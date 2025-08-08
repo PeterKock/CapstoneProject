82299af066fe263e95c22f6f44faaf4068bd72b4
pkdev <ppkock@gmail.com>
2025-08-08 10:58:17 +0200

    Create TradeAccountRepository class with internal datastore map for storing TradeAccount instances

--- repository/TradeAccountRepository.java
+++ repository/TradeAccountRepository.java
@@ -1,7 +1,17 @@
 package repository;
 
+import pojo.TradeAccount;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * Repository class for managing TradeAccount instances.
+ * Provides CRUD operations backed by an in-memory data store.
+ */
 public class TradeAccountRepository {
 
+    // In-memory store for trade accounts, keyed by account ID
+    private Map<String, TradeAccount> datastore = new HashMap<>();
 
+    // More methods will be added in the next tasks
 }
