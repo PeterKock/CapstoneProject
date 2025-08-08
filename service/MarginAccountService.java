5e21b2dae8a5e9a5b39be3b200d126dbb8d889ef
pkdev <ppkock@gmail.com>
2025-08-08 11:39:29 +0200

    Create MarginAccountService class implementing TradeAccountService with repository dependency

--- service/MarginAccountService.java
+++ service/MarginAccountService.java
@@ -1,7 +1,30 @@
 package service;
 
+import repository.TradeAccountRepository;
 
-public class MarginAccountService {
+/**
+ * Service class for handling operations on MarginAccount objects.
+ */
+public class MarginAccountService implements TradeAccountService {
 
+    // Repository for storing and managing MarginAccount instances
+    private TradeAccountRepository repository;
 
+    /**
+     * Constructor that injects the TradeAccountRepository dependency.
+     * @param repository the shared TradeAccountRepository
+     */
+    public MarginAccountService(TradeAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void deposit(String id, java.math.BigDecimal amount) {
+        
+    }
+
+    @Override
+    public void withdraw(String id, java.math.BigDecimal amount) {
+        
+    }
 }
